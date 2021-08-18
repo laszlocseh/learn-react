c38e9bb982f96f7e4421575110f2dcd4cff59aa3
laszlocseh <csehlaszlo91@gmail.com>
2021-08-18 16:02:44 +0300

    Page 13

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var coolTest = "Cool test here 2";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {coolTest}
         </p>
         <a
           className="App-link"
@@ -20,6 +26,22 @@ function App() {
       </header>
     </div>
   );
+}
+
+class App2 extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
